88a38a9978190d7987fe03d9e6e4e103cbac303f
Zbyněk Winkler <zbynek.winkler@gmail.com>
2024-03-08 22:51:17 +0100

    move dxf export to wall_a

--- notebooks/wall.py
+++ notebooks/wall.py
@@ -47,22 +47,7 @@ p = wall_a.make_part(width, height)
 show(p)
 
 # %%
-# https://cadquery.readthedocs.io/en/latest/classreference.html#cadquery.occ_impl.exporters.dxf.DxfDocument
 
-from cadquery.occ_impl.exporters.dxf import DxfDocument
-dxf = DxfDocument()
-blue = wall_a.outside(width, height).wires().offset(-0.25, mode='i').reset().clean()
-cyan = wall_a.inside(height).wires().offset(0.25, mode='a').reset().clean()
-green = wall_a.half_depth(width, height).wires().offset(0.25, mode='a').reset().clean()
-blue_w = cq.Workplane("XY").add(blue.faces().vals())
-cyan_w = cq.Workplane("XY").add(cyan.faces().vals())
-green_w = cq.Workplane("XY").add(green.faces().vals())
-dxf.add_layer("4_ANYTOOL_CUTTHROUGH_OUTSI", color=5)
-dxf.add_shape(blue_w, "4_ANYTOOL_CUTTHROUGH_OUTSI")
-dxf.add_layer("3_ANYTOOL_CUTTHROUGH_INSID", color=4)
-dxf.add_shape(cyan_w, "3_ANYTOOL_CUTTHROUGH_INSID")
-dxf.add_layer("5_ANYTOOL_HALF_MILL_9MM_IN", color=3)
-dxf.add_shape(green_w, "5_ANYTOOL_HALF_MILL_9MM_IN")
-dxf.document.saveas("SKYLARK250_WALL-M-A.dxf")
+blue_w, cyan_w, green_w = wall_a.make_cnc(width, height, '.')
 
 show(p, blue_w, cyan_w, green_w, names=["part", "blue", "cyan", "green"])
--- src/cadquery_skylark/wall_a.py
+++ src/cadquery_skylark/wall_a.py
@@ -1,4 +1,7 @@
+import pathlib
+
 import cadquery as cq
+from cadquery.occ_impl.exporters import dxf
 
 from . import details
 
@@ -89,3 +92,22 @@ def make_part(width: mm, height: mm) -> cq.Workplane:
     p = p.faces(">Z").placeSketch(half_s).cutBlind(-18 / 2)
 
     return p
+
+
+def make_cnc(width: mm, height: mm, dirpath):
+    doc = dxf.DxfDocument()
+    blue = outside(width, height).wires().offset(-0.25, mode="i").reset().clean()
+    cyan = inside(height).wires().offset(0.25, mode="a").reset().clean()
+    green = half_depth(width, height).wires().offset(0.25, mode="a").reset().clean()
+    blue_w = cq.Workplane("XY").add(blue.faces().vals())
+    cyan_w = cq.Workplane("XY").add(cyan.faces().vals())
+    green_w = cq.Workplane("XY").add(green.faces().vals())
+    doc.add_layer("4_ANYTOOL_CUTTHROUGH_OUTSI", color=5)
+    doc.add_shape(blue_w, "4_ANYTOOL_CUTTHROUGH_OUTSI")
+    doc.add_layer("3_ANYTOOL_CUTTHROUGH_INSID", color=4)
+    doc.add_shape(cyan_w, "3_ANYTOOL_CUTTHROUGH_INSID")
+    doc.add_layer("5_ANYTOOL_HALF_MILL_9MM_IN", color=3)
+    doc.add_shape(green_w, "5_ANYTOOL_HALF_MILL_9MM_IN")
+    filepath = pathlib.Path(dirpath) / "SKYLARK250_WALL-M-A.dxf"
+    doc.document.saveas(filepath)
+    return blue_w, cyan_w, green_w
